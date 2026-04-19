d9f6b1720c6d14711cb716baa29f11b3d57eafb3
Minh Sang <vosang20102005@gmail.com>
2026-04-19 18:47:34 +0700

    fix(android): remove duplicate manifest and gradle entries

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 }
 
 android {
-    namespace = "com.example.app_cand"
     namespace = "com.example.cand_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
@@ -22,7 +21,6 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.app_cand"
         applicationId = "com.example.cand_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
